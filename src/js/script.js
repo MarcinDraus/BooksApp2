bdef407e55610425c68ca4a4c5955d3ac169d71c
MarcinDraus <marcin.drauss@gmail.com>
2023-04-17 00:47:40 +0200

    10.2 exeexercise3

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,55 @@
 //'use strict';
 
 const booksListEl = document.querySelector('.books-list');
-console.log(booksListEl);
+
 const bookWrapper = document.querySelector('#template-book');
 
 
-console.log(bookWrapper);
+console.log(bookWrapper, 'bookWrapper');
 const data = dataSource.books;
 
-//   displayBooks();
-//     data.forEach(function(element){
-//      console.log(element);
-//     });
-
-/*displayBooks()*/
 function  render(){
+  //sprawdź kazdy element jeden po drugim z tablicy- dataSource.books 
+    
   for(const book of data){
-    console.log(data, book);
+    console.log(data,'data', book, 'book');
+    //wświetl id="template-book" ,  <li class="book">
     const wrapper = Handlebars.compile(bookWrapper.innerHTML);
+    //załaduj do <li> część z tablicty dataSource.books - skąd wie którą{}?
     const generateHTML = wrapper(book);
+    //wyświetl w postaci nowego <div> zawartość "<li> część z tablicty dataSource"
     const container = utils.createDOMFromHTML(generateHTML);
+    console.log(container, 'container');
+    //stworzyć nowy pod element ( dziecko) do elementu <ul> i tam włozć  zawartość "<li> część z tablicty dataSource"
     booksListEl.appendChild(container);    
+    console.log(booksListEl,'booksListEl');
   }
 }
 render();
+const favoriteBooks = [];
+function initActions(){
+  const booksImages = document.querySelectorAll('.book__image');
+  for(const bookEl of booksImages ){
+    
+    bookEl.addEventListener('dblclick', (event)=> {
+      event.preventDefault();
+      const addfavoriteBooks = bookEl.getAttribute('data-id');
+      if(bookEl.classList.contains('favorite'),
+      favoriteBooks.includes(addfavoriteBooks)
+      //favoriteBooks.indexOf(addfavoriteBooks)
+      ){
+        bookEl.classList.remove('favorite');
+        const arrIndex = favoriteBooks.indexOf(addfavoriteBooks);
+        favoriteBooks.splice(arrIndex, 1);
+      }else{
+        bookEl.classList.add('favorite');
+        favoriteBooks.push(addfavoriteBooks);
+      }
+      
+      
+      console.log(favoriteBooks);
+    });
+    
+  }
+}
+initActions();
